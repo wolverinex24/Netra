f9afdc828ab1b6372c31adc750193a5b896c86db
Vedant Fuley <vedantfuley@gmail.com>
2024-02-20 15:34:06 +0530

    folder structure updated.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:netra/features/homeScreen/screens/homeScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,133 +17,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: PredictionScreen(),
-    );
-  }
-}
-
-class PredictionScreen extends StatefulWidget {
-  @override
-  _PredictionScreenState createState() => _PredictionScreenState();
-}
-
-class _PredictionScreenState extends State<PredictionScreen> {
-  File? _image;
-  final picker = ImagePicker();
-
-  Future getImageFromCamera() async {
-    final pickedFile = await picker.pickImage(source: ImageSource.camera);
-
-    setState(() {
-      if (pickedFile != null) {
-        _image = File(pickedFile.path);
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
-
-  Future getImageFromGallery() async {
-    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-
-    setState(() {
-      if (pickedFile != null) {
-        _image = File(pickedFile.path);
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Image Prediction'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _image == null
-                ? Text('No image selected.')
-                : Image.file(
-                    _image!,
-                    width: 300,
-                    height: 300,
-                  ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: getImageFromCamera,
-              child: Text('Take Picture'),
-            ),
-            ElevatedButton(
-              onPressed: getImageFromGallery,
-              child: Text('Choose from Gallery'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (_image != null) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ResultScreen(image: _image!),
-                    ),
-                  );
-                }
-              },
-              child: Text('Submit'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ResultScreen extends StatelessWidget {
-  final File image;
-
-  ResultScreen({required this.image});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Prediction Result'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.file(
-              image,
-              width: 300,
-              height: 300,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Prediction Result Text', // You can replace this with actual prediction result
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              maxLines: 3,
-              decoration: InputDecoration(
-                hintText: 'Enter Description',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Continue Inspection'),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
